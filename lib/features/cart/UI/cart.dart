8e985e74bae65d6b8d45e7577b39bae670d6edc7
Adrian Espinola <boqbunswer@gmail.com>
2024-05-02 17:03:54 -0300

    commit

--- lib/features/cart/UI/cart.dart
+++ lib/features/cart/UI/cart.dart
@@ -11,98 +11,103 @@ class Cart extends StatefulWidget {
   @override
   State<Cart> createState() => _CartState();
 }
-  
-  class _CartState extends State<Cart> {
+
+class _CartState extends State<Cart> {
   final cartBloc = CartBloc();
   @override
   void initState() {
     cartBloc.add(CartInitialEvent());
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Carrinho', 
-        style: TextStyle(
-          fontSize: 40,
-        ) 
-        
+        appBar: AppBar(
+          title: const Text('Carrinho',
+              style: TextStyle(
+                fontSize: 40,
+              )),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  GoRouter.of(context).go('/Catalog');
+                },
+                icon: const Icon(Icons.arrow_back)),
+          ],
         ),
-        actions: [
-          IconButton(
-            onPressed: (){
-              GoRouter.of(context).go('/Catalog');
-            }, 
-            icon: const Icon(Icons.arrow_back)),
-        ],
-        
-      ),
-      body:BlocConsumer<CartBloc, CartState>(
-        bloc: cartBloc,
-        listener: (context, state) {},
-        listenWhen: (previous, current) => current is CartActionState,
-        buildWhen: (previous, current) => current is !CartActionState,
-        builder: (context, state) {
-  switch (state.runtimeType) {
-    case const (CartSuccessState):
-      final successState = state as CartSuccessState;
-      final total = cartBloc.calcularTotal(successState.cartItens);
-      return Column(
-        
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color:lightColorScheme.primary,
-            ),    
-            padding: const EdgeInsets.all(10),
-            height: 80,
-            width: double.maxFinite,
-            child: Card(
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Total:', // Formatando para exibir 2 casas decimais
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      '\$${total.toStringAsFixed(2)}', // Formatando para exibir 2 casas decimais
-                      style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: successState.cartItens.length,
-              itemBuilder: (context, index) {
-                return CartTileWidget(
-                  cartBloc: cartBloc,
-                  catalogDataModel: successState.cartItens[index],
-                );
-              },
-            ),
-          ),
-
-           SizedBox(
-            width: 400,
-             child: Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: ElevatedButton(
-                onPressed: (){}, 
-                child: const Text('Finalizar Compra')
-                ),
-             ),
-           ),
-        ],
-      );
-    default:
-      return Container();
+        body: BlocConsumer<CartBloc, CartState>(
+            bloc: cartBloc,
+            listener: (context, state) {},
+            listenWhen: (previous, current) => current is CartActionState,
+            buildWhen: (previous, current) => current is! CartActionState,
+            builder: (context, state) {
+              switch (state.runtimeType) {
+                case const (CartSuccessState):
+                  final successState = state as CartSuccessState;
+                  final total = cartBloc.calcularTotal(successState.cartItens);
+                  return Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: lightColorScheme.primary,
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        height: 80,
+                        width: double.maxFinite,
+                        child: Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text(
+                                  'Total:', // Formatando para exibir 2 casas decimais
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  '\$${total.toStringAsFixed(2)}', // Formatando para exibir 2 casas decimais
+                                  style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: successState.cartItens.length,
+                          itemBuilder: (context, index) {
+                            return CartTileWidget(
+                              cartBloc: cartBloc,
+                              catalogDataModel: successState.cartItens[index],
+                            );
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        width: 400,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                              onPressed: () {
+                                ScaffoldMessenger.of(context)
+                                    .hideCurrentSnackBar();
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(content: Text('Pagamento Confirmado')));
+                              },
+                              child: const Text('Finalizar Compra')),
+                        ),
+                      ),
+                    ],
+                  );
+                default:
+                  return Container();
+              }
+            }));
   }
 }
-    ));}}
